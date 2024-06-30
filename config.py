d2ed2c8f0a5b56982d22f3e53c785f822b2febc1
gaelan-ios <111251545+gaelan-ios@users.noreply.github.com>
2024-07-01 02:55:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "21211216"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "7404d8663463075dfd564027f32810fc" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7298821634:AAFtsKLlusHPPpdk0lJnlBnZkXgO0I5g6Yg" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
